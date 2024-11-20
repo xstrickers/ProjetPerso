f6a72e7be91731f9d861e8a8053bbc10ee5d1d63
xstrickers <clercgallien@gmail.com>
2024-11-20 04:52:05 +0100

    add offroad_car + update vehicle controls

---
+++ testMulti/Source/testMulti/testMulti.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "testMulti.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, testMulti, "testMulti" );
